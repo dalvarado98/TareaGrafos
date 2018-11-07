c2255a871254b3cf8eb7e34f2c7dc49655ad2dc1
danilocalderonolivares <dcalderono@ucenfotec.ac.cr>
2018-11-07 11:27:34 -0600

    Se agregó el proyecto de listas  multiples de adyacencia

---
+++ Listas encadenada múltiple de adyacencia/GrafoND/Vertice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrafoND
+{
+   public class Vertice
+    {
+        public TYPE Type { get; set; }
+    }
+}
